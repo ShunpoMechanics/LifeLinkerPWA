3410d59b4eba07c3583658b0f655fc7c40c3e040
Jonathan <jlc3939@gmail.com>
2020-10-05 12:00:32 -0400

    committing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { ScreenOrientation } from '@ionic-native/screen-orientation/ngx';
 
 @NgModule({
   declarations: [AppComponent],
@@ -16,7 +17,8 @@ import { AppRoutingModule } from './app-routing.module';
   providers: [
     StatusBar,
     SplashScreen,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    ScreenOrientation
   ],
   bootstrap: [AppComponent]
 })
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ScreenOrientation } from '@ionic-native/screen-orientation/ngx';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,48 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  p1Life = 40;
+  p2Life = 40;
+  p3Life = 40;
+  p4Life = 40;
 
+  constructor(private screenOrientation: ScreenOrientation) {
+    // this.screenOrientation.lock(this.screenOrientation.ORIENTATIONS.LANDSCAPE);
+  }
+
+  lowerLife(id: string) {
+    switch (id)
+    {
+      case 'p1Life':
+        this.p1Life--;
+        break;
+      case 'p2Life':
+        this.p2Life--;
+        break;
+      case 'p3Life':
+        this.p3Life--;
+        break;
+      case 'p4Life':
+        this.p4Life--;
+        break;
+    }
+  }
+
+  raiseLife(id: string) {
+    switch (id)
+    {
+      case 'p1Life':
+        this.p1Life++;
+        break;
+      case 'p2Life':
+        this.p2Life++;
+        break;
+      case 'p3Life':
+        this.p3Life++;
+        break;
+      case 'p4Life':
+        this.p4Life++;
+        break;
+    }
+  }
 }
